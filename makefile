879e174c6f4595c0707088cfe6a10f56542efb9a
camillac <ccheng6@stuy.edu>
2018-10-22 00:14:20 -0400

    0.81 for 1 mill

--- makefile
+++ makefile
@@ -8,7 +8,7 @@ sieve.o: sieve.c sieve.h
 	gcc -c sieve.c -lm
 
 driver.o: driver.c
-	gcc -c driver.c -lm 
+	gcc -c driver.c -lm
 
 clean:
 	-rm -rf *.o a.out
--- sieve.c
+++ sieve.c
@@ -9,26 +9,36 @@ int sieve(int n){
   int size;
   if (n < 1000) {
     size = n * log(n) * 1.3 + 10;
-    array = calloc(size, sizeof(int));
   }
   else {
     size = n* log(n) * 1.5;
-    array = calloc(size, sizeof(int));
   }
 
-  array[0] = 1;
-  array[1] = 1;
+  array = calloc(size, sizeof(int));
 
-  int x = 0;
-  int num = 0;
+  int x = 2;
+  int num = 1;
+  // printf("x= %d\n", x);
+  // printf("num= %d\n", num);
+  for (;x < size; x += 2){
+    // printf("cur =%d\n", x);
+    array[x]++;
+  }
+  x = 3;
   while (x < size && num < n) {
-    if (array[x] == 0){
+    if (!array[x]){
+      // printf("x= %d\n", x);
       num++;
-      for (int cur = num * 2; cur < size; cur += num) {
-        array[cur] = 1;
+      // printf("num= %d\n", num);
+      if (x < (1.15 * sqrt(n * log(n)))) {
+        for (int cur = x * x; cur < size; cur += (x * 2)) {
+          // printf("cur =%d\n", cur);
+          array[cur]++;
+        }
       }
     }
-    x++;
+    x += 2;
   }
+  return x-2;
 
 }
